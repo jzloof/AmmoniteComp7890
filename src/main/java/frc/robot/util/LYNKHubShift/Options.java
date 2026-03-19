71780d09c072d9d2b8ceeec9cfccc8e8244debb1
team7890 <team7890@gmail.com>
2026-03-19 18:29:47 -0400

    LYNK Shift Change

---
+++ src/main/java/frc/robot/util/LYNKHubShift/Options.java
@@ -0,0 +1,7 @@
+package frc.robot.util.LYNKHubShift;
+
+import frc.robot.util.LYNKHubShift.LYNKTunableOptions;
+
+public final class Options {
+    public static final LYNKTunableOptions optHubActive = new LYNKTunableOptions("Hub Active (Testing)", true);
+}
